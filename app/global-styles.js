28083551a2089439d213d43ababb04c4f062dd0d
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-03-04 23:51:26 +0200

    Minor refactor

--- app/global-styles.js
+++ app/global-styles.js
@@ -5,7 +5,6 @@ injectGlobal`
   * {
     -webkit-tap-highlight-color: transparent;
     box-sizing: border-box;
-    /* outline: thin solid red; */
   }
 
     *::-webkit-scrollbar {
--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -5,17 +5,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 // import PropTypes from 'prop-types';
+import Wrapper from './styles';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Home extends React.PureComponent {
   render() {
     return (
-      <div>
+      <Wrapper>
         <Helmet>
           <title>Home</title>
           <meta name="description" content="Description of Home" />
         </Helmet>
-      </div>
+      </Wrapper>
     );
   }
 }
