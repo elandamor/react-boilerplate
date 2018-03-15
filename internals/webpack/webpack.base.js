9a2c09af2760f5ca8b7d249f16057ae4f990d870
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-03-15 02:51:17 +0200

    Added typescript rules to base webpack config

--- internals/webpack/webpack.base.js
+++ internals/webpack/webpack.base.js
@@ -16,32 +16,40 @@ module.exports = (options) => ({
   module: {
     rules: [
       {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: options.babelQuery,
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      }, {
         test: /\.js$/, // Transform all .js files required somewhere with Babel
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: options.babelQuery,
         },
-      },
-      {
+      }, {
         // Preprocess our own .css files
         // This is the place to add your own loaders (e.g. sass/less etc.)
         // for a list of loaders, see https://webpack.js.org/loaders/#styling
         test: /\.css$/,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader'],
-      },
-      {
+      }, {
         // Preprocess 3rd party .css files located in node_modules
         test: /\.css$/,
         include: /node_modules/,
         use: ['style-loader', 'css-loader'],
-      },
-      {
+      }, {
         test: /\.(eot|otf|ttf|woff|woff2)$/,
         use: 'file-loader',
-      },
-      {
+      }, {
         test: /\.(gif|jpe?g|png|svg|webp)$/i,
         use: [
           'file-loader',
@@ -62,16 +70,13 @@ module.exports = (options) => ({
             },
           },
         ],
-      },
-      {
+      }, {
         test: /\.html$/,
         use: 'html-loader',
-      },
-      {
+      }, {
         test: /\.json$/,
         use: 'json-loader',
-      },
-      {
+      }, {
         test: /\.(mp4|webm)$/,
         use: {
           loader: 'url-loader',
@@ -107,6 +112,8 @@ module.exports = (options) => ({
       '.js',
       '.jsx',
       '.react.js',
+      '.ts',
+      '.tsx',
     ],
     mainFields: [
       'browser',
