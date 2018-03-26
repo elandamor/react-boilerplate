cd27313324c97445914d1c6a02ecb5030497f890
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-03-27 01:04:58 +0200

    Refactored App routes to seperate file

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -1,27 +1,26 @@
-/**
- * App
- */
-
-import * as React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
-// Pages
-import Home from '../../pages/Home/Loadable';
-// Styled-Components
+// Components
+import Routes from '../../components/Routes';
+// Routes
+import routes from './routes';
+// Styles
 import Wrapper from './styles';
 
+/**
+ * @render react
+ * @name App container
+ * @description The skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar).
+ */
+
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component<{}> {
+class App extends Component<{}> {
   render() {
     return (
       <Wrapper>
         <Switch>
-          <Route
-            exact
-            path="/"
-            render={(props) => (
-              <Home {...props} />
-            )}
-          />
+          <Routes routes={routes} />
         </Switch>
       </Wrapper>
     );
