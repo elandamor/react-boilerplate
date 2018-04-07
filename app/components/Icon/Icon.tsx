2704973c01609930773e879d944689cf48e4bf15
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-04-07 10:24:42 +0200

    Created Icon component

---
+++ app/components/Icon/Icon.tsx
@@ -0,0 +1,45 @@
+import React, { SFC } from 'react';
+import classNames from 'classnames';
+// Dependencies
+import ICONS from './constants';
+// Styles
+import Wrapper from './styles';
+
+/**
+ * @render react
+ * @name Icon component
+ * @description Icon component.
+ * @example
+ * <Icon
+ *  icon="star"
+ * />
+ */
+
+interface IProps {
+  className?: string;
+  icon: string;
+  size?: number;
+  viewBox?: string;
+}
+
+const Icon: SFC<IProps> = ({ className, icon, size, viewBox }) => (
+  <Wrapper
+    className={classNames('c-icon', className)}
+    aria-hidden="true"
+  >
+    <svg
+      height={`${size}`}
+      width={`${size}`}
+      viewBox={`${viewBox}`}
+    >
+      <path d={ICONS[icon]} />
+    </svg>
+  </Wrapper>
+);
+
+Icon.defaultProps = {
+  size: 24,
+  viewBox: '0 0 24 24',
+};
+
+export default Icon;
