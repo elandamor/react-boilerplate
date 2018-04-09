ecc72ec73feea14c978b695171fc7f4e5a9e38d6
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-04-10 01:02:34 +0200

    Implemented ThemeProvider

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 // Components
 import Routes from '../../components/Routes';
 // Routes
 import routes from './routes';
 // Styles
 import Wrapper from './styles';
+
+/* tslint:disable:object-literal-sort-keys */
+const themeDark = {
+  isDark: true,
+  palette: {
+    bodyBackground: '#1F1F27',
+    brandPrimary: '#25252D',
+    cardBackground: '#25252D',
+    cardBorderColor: '#414148',
+  },
+};
+
+const themeLight = {
+  isDark: false,
+  palette: {
+    bodyBackground: '#fafafa',
+    brandPrimary: '#ffffff',
+    cardBackground: '#ffffff',
+    cardBorderColor: '#E4E6E9',
+  },
+};
+/* tslint:enable:object-literal-sort-keys */
 
 /**
  * @render react
@@ -14,16 +37,30 @@ import Wrapper from './styles';
  * contain code that should be seen on all pages. (e.g. navigation bar).
  */
 
-class App extends Component<{}> {
+class App extends Component<{}, IState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: themeLight,
+    };
+  }
+
   public render() {
     return (
-      <Wrapper>
-        <Switch>
-          <Routes routes={routes} />
-        </Switch>
-      </Wrapper>
+      <ThemeProvider theme={this.state.theme}>
+        <Wrapper>
+          <Switch>
+            <Routes routes={routes} />
+          </Switch>
+        </Wrapper>
+      </ThemeProvider>
     );
   }
+}
+
+interface IState {
+  theme: object;
 }
 
 export default withRouter(App);
