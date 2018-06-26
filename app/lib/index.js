7bdb52c51efc24730e600f1baa3f5c5ae1f2c1a6
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-06-26 11:55:26 +0200

    Remove matchMedia library

--- app/lib/index.js
+++ app/lib/index.js
@@ -2,7 +2,4 @@ export { debug, makeDebugger } from './debug';
 
 export { default as isBrowser } from './isBrowser';
 export { default as shallowEqual } from './shallowEqual';
-
-export { default as matchMedia } from './matchMedia';
-
 export { default as uploader } from './uploader';
