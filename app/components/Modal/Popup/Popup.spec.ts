0afaa840d425460c3795860b1aa9968a114b8922
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-07-27 22:19:31 +0200

    Feature: Add Modal component

---
+++ app/components/Modal/Popup/Popup.spec.ts
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import Popup from '../index';
+
+describe('<Popup />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
---
+++ app/components/Modal/Popup/Popup.tsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+// Styles
+import Wrapper from './styles';
+
+/**
+ * @render react
+ * @name Popup container
+ * @description Popup container.
+ * @example
+ * <Popup />
+ */
+
+// tslint:disable-next-line:interface-over-type-literal
+type Styles = {
+  container?: object;
+  overlay?: object;
+};
+
+interface IDefaultProps {
+  className?: string;
+  styles?: Styles;
+}
+
+interface IProps extends IDefaultProps {
+  children: any;
+  onClose: () => void;
+}
+
+interface IState {
+  [key: string]: any;
+}
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Popup extends Component<IProps, IState> {
+  protected static defaultProps: IDefaultProps = {
+    styles: {
+      container: null,
+      overlay: null,
+    },
+  };
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {};
+  }
+
+  public render() {
+    const { children, className, styles } = this.props;
+
+    return (
+      <Wrapper
+        className={classNames('c-popup', className)}
+        {...(styles.container ? { style: styles.container } : {})}
+      >
+        {children}
+      </Wrapper>
+    );
+  }
+}
+
+export default Popup;
