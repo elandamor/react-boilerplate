9baa3ea23f9fa12c8264ec2c7cfe51cac3d9a96a
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-09-22 20:53:04 +0200

    Feature: Add MultiStep component test

--- app/components/MultiStep/MultiStep.spec.tsx
+++ app/components/MultiStep/MultiStep.spec.tsx
@@ -1,10 +1,29 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+// MultiStep.spec.tsx
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import 'jest-styled-components';
 
-// import MultiStep from '../index';
+import MultiStep from './index';
+
+const Step1: React.SFC<{}> = () => <div>Step 1</div>;
+const Step2: React.SFC<{}> = () => <div>Step 2</div>;
+const Step3: React.SFC<{}> = () => <div>Step 3</div>;
 
 describe('<MultiStep />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should display a MultiStep', () => {
+    const component = renderer
+      .create(
+        <MultiStep
+          onSubmit={() => null}
+          steps={[
+            { component: <Step1 />, name: 'Step 1' },
+            { component: <Step2 />, name: 'Step 2' },
+            { component: <Step3 />, name: 'Step 3' },
+          ]}
+        />,
+      )
+      .toJSON();
+
+    expect(component).toMatchSnapshot();
   });
 });
