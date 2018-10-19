f8708679c44fec02cbee7cb36ef5baa2b9f8673a
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-10-19 04:24:30 +0200

    Feature: Add Avatar and Header components to components index

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,9 @@
+export { default as Avatar } from './Avatar';
 export { default as Button } from './Button';
 export { default as Carousel } from './Carousel';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as Form } from './Form';
+export { default as Header } from './Header';
 export { default as Icon } from './Icon';
 export { default as Image } from './Image';
 export { default as Input } from './Input';
