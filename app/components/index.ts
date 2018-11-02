79363e33907c85c4c28b4ef1c4e422ab82b97eb1
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-02 09:07:16 +0200

    Feature: Add LoadingBar component to components index

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as Image } from './Image';
+export { default as LoadingBar } from './LoadingBar';
 export { default as Navigation } from './Navigation';
 export { default as Routes } from './Routes';
