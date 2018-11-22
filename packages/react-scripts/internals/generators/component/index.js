a57dc044cb5313d5d6fa33ffadcf2311e7d1e141
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-22 14:50:37 +0200

    Initial commit

---
+++ packages/react-scripts/internals/generators/component/index.js
@@ -0,0 +1,83 @@
+/**
+ * Component Generator
+ */
+
+/* eslint strict: ["off"] */
+
+'use strict';
+
+const componentExists = require('../utils/componentExists');
+
+module.exports = {
+  description: 'Add an unconnected component',
+  prompts: [{
+    type: 'list',
+    name: 'type',
+    message: 'Select the type of component',
+    default: 'Stateless Function',
+    choices: () => ['Stateless Function', 'PureComponent', 'Component'],
+  }, {
+    type: 'input',
+    name: 'name',
+    message: 'What should it be called?',
+    default: 'Button',
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return componentExists(value) ? 'A component or container with this name already exists' : true;
+      }
+
+      return 'The name is required';
+    },
+  }, {
+    type: 'input',
+    name: 'tag',
+    message: 'What tag is it?',
+    default: 'div',
+  }, {
+    type: 'confirm',
+    name: 'wantLoadable',
+    default: false,
+    message: 'Do you want to load the component asynchronously?',
+  }],
+  actions: (data) => {
+    let componentTemplate;
+
+    switch (data.type) {
+      case 'Stateless Function': {
+        componentTemplate = './component/stateless.hbs';
+        break;
+      }
+      default: {
+        componentTemplate = './component/class.hbs';
+      }
+    }
+
+    const actions = [{
+      type: 'add',
+      path: '../../app/components/{{properCase name}}/index.tsx',
+      templateFile: componentTemplate,
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: '../../app/components/{{properCase name}}/{{properCase name}}.spec.tsx',
+      templateFile: './component/spec.hbs',
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: '../../app/components/{{properCase name}}/styles.ts',
+      templateFile: './component/styles.hbs',
+    }];
+
+    // If want Loadable.ts to load the component asynchronously
+    if (data.wantLoadable) {
+      actions.push({
+        type: 'add',
+        path: '../../app/components/{{properCase name}}/Loadable.ts',
+        templateFile: './component/loadable.hbs',
+        abortOnFail: true,
+      });
+    }
+
+    return actions;
+  },
+};
---
+++ packages/react-scripts/internals/generators/page/index.js
@@ -0,0 +1,55 @@
+/**
+ * Container Generator
+ */
+
+const componentExists = require('../utils/componentExists');
+
+module.exports = {
+  description: 'Add a container component',
+  prompts: [{
+    type: 'list',
+    name: 'type',
+    message: 'Select the base component type:',
+    default: 'PureComponent',
+    choices: () => ['PureComponent', 'Component'],
+  }, {
+    type: 'input',
+    name: 'name',
+    message: 'What should it be called?',
+    default: 'Home',
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return componentExists(value) ? 'A page with this name already exists' : true;
+      }
+
+      return 'The name is required';
+    },
+  }],
+  actions: () => {
+    const componentTemplate = './page/index.hbs';
+
+    const actions = [{
+      type: 'add',
+      path: '../../app/pages/{{properCase name}}/index.tsx',
+      templateFile: componentTemplate,
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: '../../app/pages/{{properCase name}}/{{properCase name}}.spec.tsx',
+      templateFile: './page/spec.hbs',
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: '../../app/pages/{{properCase name}}/Loadable.ts',
+      templateFile: './component/loadable.hbs',
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: '../../app/pages/{{properCase name}}/styles.ts',
+      templateFile: './page/styles.hbs',
+      abortOnFail: true,
+    }];
+
+    return actions;
+  },
+};
