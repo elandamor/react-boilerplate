321e7a35a08bfcb6903c1717e9070de4e9386ca7
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-23 08:38:49 +0200

    Refactor: build and generate scripts Should make paths to scripts relative

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -1,9 +1,12 @@
 const { execSync } = require('child_process');
+const { relative } = require('path');
+
+const script = relative(process.cwd(), 'internals/webpack/webpack.prod.js');
 
 function initBuild() {
   try {
     execSync(
-      'cross-env NODE_ENV=production webpack --config internals/webpack/webpack.prod.js --color -p --progress --hide-modules --display-optimization-bailout',
+      `cross-env NODE_ENV=production webpack --config ${script} --color -p --progress --hide-modules --display-optimization-bailout`,
       { stdio: 'inherit' }
     );
     return true;
--- packages/react-scripts/scripts/generate.js
+++ packages/react-scripts/scripts/generate.js
@@ -1,11 +1,13 @@
 const { execSync } = require('child_process');
+const { relative } = require('path');
 
 const args = process.argv.slice(2);
+const script = relative(process.cwd(), 'internals/generators/index.js');
 
 function initGenerator() {
   try {
     execSync(
-      `plop --plopfile internals/generators/index.js ${args[1] || ''}`,
+      `plop --plopfile ${script} ${args[1] || ''}`,
       { stdio: 'inherit' }
     );
     return true;
