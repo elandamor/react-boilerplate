e0688e2dbad736774b5f04dd489b2dd512991b37
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-24 07:13:34 +0200

    Fix: codacy issues

--- packages/react-scripts/utils/crossSpawn.js
+++ packages/react-scripts/utils/crossSpawn.js
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
-
-var crossSpawn = require('cross-spawn');
+const crossSpawn = require('cross-spawn');
 
 module.exports = crossSpawn;
