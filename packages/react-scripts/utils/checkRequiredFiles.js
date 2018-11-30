bb88519febe795d0219bcf1dce424d46013576c8
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-30 09:55:34 +0200

    Feature: Add options arg to checkRequiredFiles utility

--- packages/react-scripts/utils/checkRequiredFiles.js
+++ packages/react-scripts/utils/checkRequiredFiles.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
-function checkRequiredFiles(files) {
+function checkRequiredFiles(files, options = {}) {
   let currentFilePath;
   try {
     files.forEach((filePath) => {
@@ -21,9 +21,11 @@ function checkRequiredFiles(files) {
   } catch (err) {
     const dirName = path.dirname(currentFilePath);
     const fileName = path.basename(currentFilePath);
-    console.log(chalk.red('Could not find a required file.'));
-    console.log(chalk.red('  Name: ') + chalk.cyan(fileName));
-    console.log(chalk.red('  Searched in: ') + chalk.cyan(dirName));
+    if (!options.silent) {
+      console.log(chalk.red('Could not find a required file.'));
+      console.log(chalk.red('  Name: ') + chalk.cyan(fileName));
+      console.log(chalk.red('  Searched in: ') + chalk.cyan(dirName));
+    }
     return false;
   }
 }
