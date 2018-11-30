5bae1771644896633aacfa0049915be03a14cc9b
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-11-30 17:10:52 +0200

    Merge pull request #59 from elandamor/develop
    Merge branch "develop" into "master"

--- packages/react-boilerplate/app/containers/App/App.spec.tsx
+++ packages/react-boilerplate/app/containers/App/App.spec.tsx
@@ -1,13 +1,18 @@
 // App.spec.tsx
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'jest-styled-components';
 
 import App from './index';
 
 describe('<App />', () => {
   it('should render App without crashing', () => {
-    const component = renderer.create(<App />).toJSON();
+    const component = renderer.create(
+      <Router>
+        <App />
+      </Router>
+    ).toJSON();
 
     expect(component).toMatchSnapshot();
   });
