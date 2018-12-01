ae514dfcdc12a63120b4e3a9695cb78ab4111d8e
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-12-01 14:19:20 +0200

    Other: Cleanup

--- packages/react-scripts/template/app/lib/index.ts
+++ packages/react-scripts/template/app/lib/index.ts
@@ -1,6 +1,4 @@
-export { default as calculatePosition } from './calculatePosition';
 export { debug, makeDebugger } from './debug';
 
 export { default as isBrowser } from './isBrowser';
 export { default as shallowEqual } from './shallowEqual';
-export { default as uploader } from './uploader';
