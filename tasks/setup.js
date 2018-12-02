e05eb20b1b8cfc4919d204e852683ed29b88760c
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-12-02 13:41:26 +0200

    Fix: eslint issues with setup task

--- tasks/setup.js
+++ tasks/setup.js
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
+/* eslint-disable no-console */
 
 const fs = require('fs');
 const path = require('path');
@@ -26,7 +26,7 @@ const handleExit = () => {
   process.exit();
 };
 
-const handleError = e => {
+const handleError = (e) => {
   console.error('ERROR! An error was encountered while executing');
   console.error(e);
   cleanup();
@@ -61,36 +61,36 @@ if (gitStatus.trim() !== '') {
 const rootDir = path.join(__dirname, '..');
 const packagesDir = path.join(rootDir, 'packages');
 const packagePathsByName = {};
-fs.readdirSync(packagesDir).forEach(name => {
+fs.readdirSync(packagesDir).forEach((name) => {
   const packageDir = path.join(packagesDir, name);
   const packageJson = path.join(packageDir, 'package.json');
   if (fs.existsSync(packageJson)) {
     packagePathsByName[name] = packageDir;
   }
 });
-Object.keys(packagePathsByName).forEach(name => {
+Object.keys(packagePathsByName).forEach((name) => {
   const packageJson = path.join(packagePathsByName[name], 'package.json');
   const json = JSON.parse(fs.readFileSync(packageJson, 'utf8'));
-  Object.keys(packagePathsByName).forEach(otherName => {
+  Object.keys(packagePathsByName).forEach((otherName) => {
     if (json.dependencies && json.dependencies[otherName]) {
-      json.dependencies[otherName] = 'file:' + packagePathsByName[otherName];
+      json.dependencies[otherName] = `file:${packagePathsByName[otherName]}`;
     }
     if (json.devDependencies && json.devDependencies[otherName]) {
-      json.devDependencies[otherName] = 'file:' + packagePathsByName[otherName];
+      json.devDependencies[otherName] = `file:${packagePathsByName[otherName]}`;
     }
     if (json.peerDependencies && json.peerDependencies[otherName]) {
       json.peerDependencies[otherName] =
-        'file:' + packagePathsByName[otherName];
+        `file:${packagePathsByName[otherName]}`;
     }
     if (json.optionalDependencies && json.optionalDependencies[otherName]) {
       json.optionalDependencies[otherName] =
-        'file:' + packagePathsByName[otherName];
+        `file:${packagePathsByName[otherName]}`;
     }
   });
 
   fs.writeFileSync(packageJson, JSON.stringify(json, null, 2), 'utf8');
   console.log(
-    'Replaced local dependencies in packages/' + name + '/package.json'
+    `Replaced local dependencies in packages/${name}/package.json`
   );
 });
 console.log('Replaced all local dependencies for testing.');
