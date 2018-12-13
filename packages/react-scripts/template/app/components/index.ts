ef26500127aeef24d7f5ba8f468b88fe2455a745
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-12-13 20:54:31 +0200

    Chore: Cleanup components

--- packages/react-scripts/template/app/components/index.ts
+++ packages/react-scripts/template/app/components/index.ts
@@ -1,6 +1,8 @@
 export { default as Button } from './Button';
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as Grid } from './Grid';
 export { default as Image } from './Image';
+export { default as Input } from './Input';
 export { default as LoadingBar } from './LoadingBar';
 export { default as Navigation } from './Navigation';
 export { default as Routes } from './Routes';
