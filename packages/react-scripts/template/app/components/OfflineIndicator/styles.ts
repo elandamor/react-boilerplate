288a7d2b0b2b15f52b9d9f7760f3499a2bae9a54
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-12-21 12:16:05 +0200

    Design + Update: OfflineIndicator component

--- packages/react-scripts/template/app/components/OfflineIndicator/styles.ts
+++ packages/react-scripts/template/app/components/OfflineIndicator/styles.ts
@@ -3,19 +3,17 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   align-items: center;
   display: flex;
-  height: 100vh;
+  height: 64px;
   justify-content: center;
-  left: 0;
   position: fixed;
   top: 0;
-  width: 100vw;
+  right: 0;
+  width: 64px;
 
   svg {
     color: ${({ theme }) => theme.palette.cardBorderColor};
-    height: 20vw;
-    max-height: 64px;
-    max-width: 64px;
-    width: 20vw;
+    height: 24px;
+    width: 24px;
   }
 `;
 
