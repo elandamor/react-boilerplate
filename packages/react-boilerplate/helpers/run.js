8b1e19cf1e6d32d65752ae278f38b31adc1c7430
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-02-09 16:12:38 +0200

    Refactor: Dependency generation For better maintainability copy over dependencies from react-scripts

--- packages/react-boilerplate/helpers/run.js
+++ packages/react-boilerplate/helpers/run.js
@@ -3,6 +3,10 @@ const chalk = require('chalk');
 const fs = require('fs-extra');
 const path = require('path');
 
+// Extract the dependencies from react-scripts (which is a workspace)
+const reactScriptsDevDependencies = require('pd-react-scripts/package.json')
+  .devDependencies;
+
 const checkIfOnline = require('./checkIfOnline');
 const checkNodeVersion = require('./checkNodeVersion');
 const executeNodeScript = require('./executeNodeScript');
@@ -11,34 +15,9 @@ const getPackageName = require('./getPackageName');
 const install = require('./install');
 const setCaretRangeForRuntimeDeps = require('./setCaretRangeForRuntimeDeps');
 
-// TODO: Dependencies should be in their own file, for better maintainability
-const dependencies = [
-  'apollo-cache-inmemory@1.3.12',
-  'apollo-cache-persist@0.1.1',
-  'apollo-client@2.4.8',
-  'apollo-link@1.2.6',
-  'apollo-link-http@1.5.9',
-  'apollo-link-state@0.4.2',
-  'classnames@2.2.6',
-  'graphql@14.0.2',
-  'history@4.7.2',
-  'hoist-non-react-statics@3.2.1',
-  'intl@1.2.5',
-  'localforage@1.7.3',
-  'react@16.7.0',
-  'react-apollo@2.3.3',
-  'react-dom@16.7.0',
-  'react-feather@1.1.5',
-  'react-helmet@5.2.0',
-  'react-measure@2.1.3',
-  'react-router-dom@4.3.1',
-  'sanitize.css@8.0.0',
-  'styled-components@4.1.3',
-  'typeface-merriweather@0.0.54',
-  'typeface-montserrat@0.0.54',
-  'whatwg-fetch@3.0.0',
-];
-
+const dependencies = Object.keys(reactScriptsDevDependencies).map(
+  (dep) => `${dep}@${reactScriptsDevDependencies[dep]}`
+);
 const devDependencies = ['pd-react-scripts'];
 
 function run(
@@ -61,10 +40,7 @@ function run(
   console.log();
   getPackageName(packageToInstall)
     .then((packageName) =>
-      checkIfOnline(useYarn).then((isOnline) => ({
-        isOnline,
-        packageName,
-      }))
+      checkIfOnline(useYarn).then((isOnline) => ({ isOnline, packageName }))
     )
     .then((info) => {
       const { isOnline, packageName } = info;
