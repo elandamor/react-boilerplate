b765f026540daa0a242930da26bebf658a78fef3
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-03-28 19:57:50 +0200

    refactor(global-styles): Update theme

--- packages/react-scripts/template/app/global-styles.ts
+++ packages/react-scripts/template/app/global-styles.ts
@@ -1,11 +1,70 @@
 import { createGlobalStyle } from 'styled-components';
 import 'typeface-roboto';
 
+export const GENERATORS = {
+  spacing: (length: number, baseline: number) =>
+    Array.from({ length }, (_, i: number) => baseline * ++i),
+};
+
+export const LAYOUT = {
+  baseline: 8, // dp
+};
+
 export const THEME = {
-  palette: {
+  borders: [
+    0,
+    '1px solid',
+    '2px solid',
+    '4px solid',
+    '8px solid',
+    '16px solid',
+    '32px solid',
+  ],
+  breakpoints: [32, 48, 64], // em
+  colors: {
+    background: '#FFFFFF',
+    primary: '#2D68EE',
     error: '#B00020',
+    success: '#00C851',
+    surface: '#FFFFFF',
+    white: '#FFFFFF',
+
+    blacks: [
+      'rgba(0,0,0,.0125)',
+      'rgba(0,0,0,.025)',
+      'rgba(0,0,0,.05)',
+      'rgba(0,0,0,.1)',
+      'rgba(0,0,0,.2)',
+      'rgba(0,0,0,.3)',
+      'rgba(0,0,0,.4)',
+      'rgba(0,0,0,.5)',
+      'rgba(0,0,0,.6)',
+      'rgba(0,0,0,.7)',
+      'rgba(0,0,0,.8)',
+      'rgba(0,0,0,.9)',
+    ],
+    whites: [
+      'rgba(255,255,255,.0125)',
+      'rgba(255,255,255,.025)',
+      'rgba(255,255,255,.05)',
+      'rgba(255,255,255,.1)',
+      'rgba(255,255,255,.2)',
+      'rgba(255,255,255,.3)',
+      'rgba(255,255,255,.4)',
+      'rgba(255,255,255,.5)',
+      'rgba(255,255,255,.6)',
+      'rgba(255,255,255,.7)',
+      'rgba(255,255,255,.8)',
+      'rgba(255,255,255,.9)',
+    ],
   },
-  spacing: [8,16,24,32,40,48,56,64,72,80],
+  fonts: {
+    sansSerif: `'Roboto',-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen
+    ,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif`,
+  },
+  fontSizes: [12, 14, 16, 20, 24, 36, 48, 80, 96],
+  fontWeights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  space: [0].concat(GENERATORS.spacing(10, LAYOUT.baseline)),
 };
 
 export default createGlobalStyle`
@@ -16,27 +75,23 @@ export default createGlobalStyle`
 
   *::-webkit-scrollbar {
     width: 0px !important;
-    height: 2px !important
+    height: 2px !important;
   }
 
   *::-webkit-scrollbar-thumb {
-    background-color: rgba(0,0,0,0.2)
+    background-color: ${THEME.colors.blacks[1]};
   }
 
   *::-webkit-scrollbar-track {
-    background: rgba(255,255,255,0.08)
-  }
-
-  :root {
-    --body-background: ${(props: any) => props.theme.palette.backgroundColor};
-    --brand-success: #34e79a;
+    background: ${THEME.colors.whites[3]};
   }
 
   html,
   body {
     -moz-osx-font-smoothing: grayscale;
     -webkit-font-smoothing: antialiased;
-    background-color: var(--body-background);
+    background-color: ${THEME.colors.background};
+    font-family: ${THEME.fonts.sansSerif};
     font-feature-settings: 'liga', 'kern' 1;
     font-kerning: normal;
     font-weight: 500;
@@ -51,8 +106,9 @@ export default createGlobalStyle`
     overflow-x: hidden;
   }
 
-  body {
-    font-family: 'Roboto',-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen
-      ,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+  [sronly], .sr-only {
+    height: 0;
+    opacity: 0;
+    position: absolute;
   }
 `;
