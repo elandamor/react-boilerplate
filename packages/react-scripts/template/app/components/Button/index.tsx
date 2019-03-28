3a4730d918e9d61d4f75f016b7869bc3f5720507
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-03-28 19:54:44 +0200

    refactor(Button): Improve component

--- packages/react-scripts/template/app/components/Button/index.tsx
+++ packages/react-scripts/template/app/components/Button/index.tsx
@@ -7,14 +7,17 @@ export interface IButtonProps {
   backgroundColor?: string;
   className?: string;
   disabled?: boolean;
-  icon?: any;
+  icon?: JSX.Element;
   iconPosition?: string;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+  marginTop?: number;
   onClick?: (event: MouseEvent<HTMLElement>) => void;
-  text?: string;
-  textColor?: string;
-  // types
   outlined?: boolean;
   raised?: boolean;
+  text?: string;
+  textColor?: string;
 }
 
 /**
@@ -36,14 +39,20 @@ const Button: FC<IButtonProps> = ({
     onClick={handleClick}
     {...rest}
   >
-    {rest.icon && rest.iconPosition !== 'right' && <i>{rest.icon}</i>}
+    {rest.icon && rest.iconPosition === 'left' && <i>{rest.icon}</i>}
     <label>{text}</label>
     {rest.icon && rest.iconPosition === 'right' && <i>{rest.icon}</i>}
   </Wrapper>
 );
 
 Button.defaultProps = {
+  iconPosition: 'left',
+  marginBottom: 0,
+  marginLeft: 0,
+  marginRight: 0,
+  marginTop: 0,
   onClick: () => null,
+  text: 'Button',
 };
 
 export default Button;
--- packages/react-scripts/template/app/components/Button/styles.ts
+++ packages/react-scripts/template/app/components/Button/styles.ts
@@ -1,22 +1,25 @@
 import styled, { css } from 'styled-components';
-import { IButtonProps } from '.';
+import { IButtonProps } from './index';
 import { THEME } from '../../global-styles';
 
 const Wrapper = styled.button`
   align-items: center;
   background: transparent;
   border: none;
-  border-radius: ${THEME.spacing[0] / 2}px;
+  border-radius: ${THEME.space[1] / 2}px;
   cursor: ${({ disabled }: IButtonProps) => disabled ? 'not-allowed' : 'pointer'};
   display: flex;
-  font-size: 14px;
+  font-size: ${THEME.fontSizes[1]}px;
   justify-content: center;
   letter-spacing: .0892857143em;
-  min-height: ${THEME.spacing[3] + (THEME.spacing[0] / 2)}px;
-  min-width: ${THEME.spacing[7]}px;
+  margin: ${({ marginTop, marginRight, marginBottom, marginLeft }: IButtonProps) =>
+    `${marginTop}px ${marginRight}px ${marginBottom}px ${marginLeft}px`
+  };
+  min-height: ${THEME.space[4] + (THEME.space[1] / 2)}px;
+  min-width: ${THEME.space[8]}px;
   opacity: ${({ disabled, raised }: IButtonProps) => raised && disabled ? '0.38' : '1'};
   outline: none;
-  padding: ${THEME.spacing[0]}px;
+  padding: ${THEME.space[1]}px;
   position: relative;
   text-transform: uppercase;
   z-index: 1;
@@ -30,13 +33,17 @@ const Wrapper = styled.button`
     pointer-events: none;
     z-index: 1;
 
-    ${({ icon, iconPosition }: IButtonProps) => icon && iconPosition !== 'right' && css`
-      margin-left: ${THEME.spacing[0]}px;
-    `};
+    ${({ icon, iconPosition }: IButtonProps) =>
+      icon && iconPosition !== 'right' && css`
+        margin-left: ${THEME.space[1]}px;
+      `
+    };
 
-    ${({ icon, iconPosition }: IButtonProps) => icon && iconPosition === 'right' && css`
-      margin-right: ${THEME.spacing[0]}px;
-    `};
+    ${({ icon, iconPosition }: IButtonProps) =>
+      icon && iconPosition === 'right' && css`
+        margin-right: ${THEME.space[1]}px;
+      `
+    };
   }
 
   &:after, &:before {
@@ -56,13 +63,13 @@ const Wrapper = styled.button`
     &:focus {
       &:before {
         ${({ raised }: IButtonProps) => !raised && css`
-          background-color: rgba(0,0,0,0.08);
+          background-color: ${THEME.colors.blacks[3]};
         `};
       }
 
       &:after {
         ${({ raised }: IButtonProps) => raised && css`
-          background-color: rgba(255,255,255,0.08);
+          background-color: ${THEME.colors.whites[3]};
         `};
       }
     }
@@ -70,48 +77,56 @@ const Wrapper = styled.button`
     &:hover {
       &:before {
         ${({ raised }: IButtonProps) => !raised && css`
-          background-color: rgba(0,0,0,0.04);
+          background-color: ${THEME.colors.blacks[2]};
         `};
       }
 
       &:after {
         ${({ raised }: IButtonProps) => raised && css`
-          background-color: rgba(0,0,0,0.04);
+          background-color: ${THEME.colors.blacks[2]};
         `};
       }
     }
   }
 
   ${({ outlined }: IButtonProps) => outlined && css`
-    border: 1px solid rgba(0,0,0,0.12);
-    padding: ${THEME.spacing[0]}px ${THEME.spacing[1]}px;
+    border: ${THEME.borders[1]} ${THEME.colors.blacks[3]};
+    padding: ${THEME.space[1]}px ${THEME.space[2]}px;
   `};
 
   ${({ backgroundColor, raised, textColor }: IButtonProps) => raised && css`
-    background-color: ${backgroundColor || '#2D68EE'};
+    background-color: ${backgroundColor || THEME.colors.primary};
     box-shadow:
-      0 3px 1px -2px rgba(0,0,0,.2),
-      0 2px 2px 0 rgba(0,0,0,.14),
-      0 1px 5px 0 rgba(0,0,0,.12);
-    color: ${textColor || '#FFFFFF'};
-    padding: ${THEME.spacing[0]}px ${THEME.spacing[1]}px;
+      0 3px 1px -2px ${THEME.colors.blacks[4]},
+      0 2px 2px 0 ${THEME.colors.blacks[3]},
+      0 1px 5px 0 ${THEME.colors.blacks[3]};
+    color: ${textColor || THEME.colors.white};
+    padding: ${THEME.space[1]}px ${THEME.space[2]}px;
   `};
 
-  ${({ icon, iconPosition }: IButtonProps) => icon && iconPosition !== 'right'  && css`
-    padding-left: ${THEME.spacing[0] / 2}px;
-  `};
+  ${({ icon, iconPosition }: IButtonProps) =>
+    icon && iconPosition !== 'right'  && css`
+      padding-left: ${THEME.space[1] / 2}px;
+    `
+  };
 
-  ${({ icon, iconPosition }: IButtonProps) => icon && iconPosition === 'right'  && css`
-    padding-right: ${THEME.spacing[0] / 2}px;
-  `};
+  ${({ icon, iconPosition }: IButtonProps) =>
+    icon && iconPosition === 'right'  && css`
+      padding-right: ${THEME.space[1] / 2}px;
+    `
+  };
 
-  ${({ icon, iconPosition, outlined, raised }: IButtonProps) => icon && iconPosition !== 'right' && (outlined || raised) && css`
-    padding-left: ${THEME.spacing[0] + (THEME.spacing[0] / 2)}px;
-  `};
+  ${({ icon, iconPosition, outlined, raised }: IButtonProps) =>
+    icon && iconPosition !== 'right' && (outlined || raised) && css`
+      padding-left: ${THEME.space[1] + (THEME.space[1] / 2)}px;
+    `
+  };
 
-  ${({ icon, iconPosition, outlined, raised }: IButtonProps) => icon && iconPosition === 'right' && (outlined || raised) && css`
-    padding-right: ${THEME.spacing[0] + (THEME.spacing[0] / 2)}px;
-  `};
+  ${({ icon, iconPosition, outlined, raised }: IButtonProps) =>
+    icon && iconPosition === 'right' && (outlined || raised) && css`
+      padding-right: ${THEME.space[1] + (THEME.space[1] / 2)}px;
+    `
+  };
 `;
 
 export default Wrapper;
