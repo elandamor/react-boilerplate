c3ff4aae13828a7694e35b02a1cb8f10856c0626
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-04-09 12:28:11 +0200

    refactor(global-styles.ts): Update theme

--- packages/react-scripts/template/app/global-styles.ts
+++ packages/react-scripts/template/app/global-styles.ts
@@ -23,11 +23,17 @@ export const THEME = {
   breakpoints: [32, 48, 64], // em
   colors: {
     background: '#FFFFFF',
+    black: '#000000',
     primary: '#2D68EE',
+    primaryDark: '#003FBB',
+    primaryLight: '#7395FF',
     error: '#B00020',
     success: '#00C851',
     surface: '#FFFFFF',
     white: '#FFFFFF',
+    cardBackground: '#FFFFFF',
+    cardBorderColor: '#E4E6E9',
+    sidebarBackground: '#F7F7FA',
 
     blacks: [
       'rgba(0,0,0,.0125)',
@@ -62,8 +68,12 @@ export const THEME = {
     sansSerif: `'Roboto',-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen
     ,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif`,
   },
-  fontSizes: [12, 14, 16, 20, 24, 36, 48, 80, 96],
+  fontSizes: [10, 12, 14, 16, 20, 24, 34, 48, 60, 96],
   fontWeights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  goldenRatio: 1.618,
+  letterSpacings: [-1.5, -0.5, 0, 0.1, 0.15, 0.25, 0.4, 0.5, 1.25, 1.5],
+  maxWidths: [320],
+  minWidths: [320],
   space: [0].concat(GENERATORS.spacing(10, LAYOUT.baseline)),
 };
 
@@ -71,6 +81,7 @@ export default createGlobalStyle`
   * {
     -webkit-tap-highlight-color: transparent;
     box-sizing: border-box;
+    /* outline: thin dashed red; */
   }
 
   *::-webkit-scrollbar {
@@ -104,6 +115,11 @@ export default createGlobalStyle`
 
   html {
     overflow-x: hidden;
+  }
+
+  [href] {
+    color: ${THEME.colors.primaryDark};
+    text-decoration: none;
   }
 
   [sronly], .sr-only {
