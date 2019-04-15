b44d5315f1d21297f53c02c4cfe3da61ff32844e
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-04-15 12:33:29 +0200

    refactor(Image): Update stories

--- packages/react-scripts/template/src/components/Image/Image.stories.tsx
+++ packages/react-scripts/template/src/components/Image/Image.stories.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Share } from 'react-feather';
 
-import Button from './index';
+import Image from './index';
 
-storiesOf('Button', module)
-.add('default', () => <Button />)
-.add('default with icon', () => <Button icon={<Share />} />)
-.add('outlined', () => <Button outlined />)
-.add('outlined with icon', () => <Button outlined icon={<Share />} />)
-.add('raised', () => <Button raised />)
-.add('raised with icon', () => <Button raised icon={<Share />} />)
+storiesOf('Image', module)
+.add('default', () => <Image src="../images" />)
 
