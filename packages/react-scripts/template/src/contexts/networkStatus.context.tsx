e4e12c773dffa34f5f3a95445ffcbf1172dc0301
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-04-16 09:54:24 +0200

    refactor(networkStatus.context): Update context

--- packages/react-scripts/template/src/contexts/networkStatus.context.tsx
+++ packages/react-scripts/template/src/contexts/networkStatus.context.tsx
@@ -1,123 +1,52 @@
-import React from 'react';
-import { compose, graphql } from 'react-apollo';
+import React, { FC, useEffect, useState } from 'react';
 // Components
 import { OfflineIndicator } from '../components';
-// Queries
-import getNetworkStatusGQL from '../graphql/queries/getNetworkStatus.gql';
-
-import client from '../apollo.config';
-
-// import { makeDebugger } from '../utils';
-// const debug = makeDebugger('networkStatus-context');
 
 const DEFAULT_STATE = {
-  networkStatus: {
-    __typename: 'NetworkStatus',
-    isConnected: true,
-  },
-  setNetworkStatus: (status: string) => {},
+  isConnected: true,
 };
 
 export const NetworkStatusContext = React.createContext(DEFAULT_STATE);
 
 interface IProps {
-  networkStatus?: string;
-  children: any;
+  children: React.ReactNode;
 }
 
-// TODO: Make sure (on/off)Line status works cross-browser
+const Provider: FC<IProps> = (props) => {
+  const [isConnected, setIsConnected] = useState(DEFAULT_STATE.isConnected);
 
-class Provider extends React.Component<IProps, {}> {
-  getNetworkStatus = true;
-  state = DEFAULT_STATE;
-
-  public componentDidMount() {
-    if (navigator.onLine) {
-      this.online();
-    } else {
-      this.offline();
-    }
-
-    window.addEventListener('online', () => this.online());
-    window.addEventListener('offline', () => this.offline());
-  }
-
-  public componentWillUnmount() {
-    window.removeEventListener('online', () => this.online());
-    window.removeEventListener('offline', () => this.offline());
-  }
-
-  public setNetworkStatus = (networkStatus: string) => {
-    this.getNetworkStatus = false;
-
-    if (networkStatus) {
-      client.writeData({
-        data: {
-          networkStatus: {
-            __typename: 'NetworkStatus',
-            isConnected: Boolean(networkStatus === 'online'),
-          },
-        },
-      });
-    }
-
-    this.setState({
-      networkStatus: {
-        __typename: 'NetworkStatus',
-        isConnected: Boolean(networkStatus === 'online'),
-      },
-    });
+  const setNetworkStatus = (networkStatus: string) => {
+    setIsConnected(Boolean(networkStatus === 'online'));
   };
 
-  public render() {
-    return (
-      <NetworkStatusContext.Provider
-        value={{
-          ...this.state,
-          setNetworkStatus: this.setNetworkStatus,
-        }}
-      >
-        <React.Fragment>
-          {this.props.children}
-          {!this.state.networkStatus.isConnected && <OfflineIndicator />}
-        </React.Fragment>
-      </NetworkStatusContext.Provider>
-    );
+  function offline() {
+    setNetworkStatus('offline');
   }
 
-  private offline() {
-    this.setNetworkStatus('offline');
+  function online() {
+    setNetworkStatus('online');
   }
 
-  private online() {
-    this.setNetworkStatus('online');
-  }
-}
+  useEffect(() => {
+    if (navigator.onLine) { online() } else { offline() };
 
-const mapNetworkStatusToProps = ({ data }: any) => {
-  if (data.error) {
-    return {
-      loading: data.loading,
-      error: data.error,
+    window.addEventListener('online', () => online());
+    window.addEventListener('offline', () => offline());
+
+    return () => {
+      window.removeEventListener('online', () => online());
+      window.removeEventListener('offline', () => offline());
     };
-  }
+  }, []);
 
-  if (!data.networkStatus) {
-    return {
-      loading: data.loading,
-    };
-  }
-
-  const { networkStatus } = data;
-
-  return {
-    networkStatus,
-    loading: data.loading,
-  };
+  return (
+    <NetworkStatusContext.Provider value={{ isConnected }}>
+      <React.Fragment>
+        {props.children}
+        {!isConnected && <OfflineIndicator />}
+      </React.Fragment>
+    </NetworkStatusContext.Provider>
+  );
 };
 
-export default compose(
-  graphql(getNetworkStatusGQL, {
-    props: mapNetworkStatusToProps,
-  }),
-)(Provider);
+export default Provider;
