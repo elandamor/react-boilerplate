984b79743d17a43b2df8c975c90a16cdae414e60
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-05-05 10:19:34 +0200

    refactor(hooks): Introduce useRouter hook

--- packages/react-scripts/template/src/hooks/index.ts
+++ packages/react-scripts/template/src/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useFormInput } from './useFormInput';
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useMedia } from './useMedia';
 export { default as usePreviousLocation } from './usePreviousLocation';
+export { default as useRouter } from './useRouter';
