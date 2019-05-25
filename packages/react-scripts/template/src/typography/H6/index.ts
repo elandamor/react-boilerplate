b4dfff34231b612296ea5779486843b0aee5d480
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-05-25 09:50:23 +0200

    refactor(typography): Introduce styled-system component types.

--- packages/react-scripts/template/src/typography/H6/index.ts
+++ packages/react-scripts/template/src/typography/H6/index.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { StyledSystemProps } from 'styled-system';
-import { headingStyles } from '../index';
+import { typography } from '@app/theme/componentTypes';
 
 /**
  * @render react
@@ -11,7 +11,7 @@ import { headingStyles } from '../index';
  */
 
 const H6 = styled.h6<StyledSystemProps>`
-  ${headingStyles};
+  ${typography};
 `;
 
 H6.defaultProps = {
